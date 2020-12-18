bf5f5c67a3f1422c5ad2565df3a0862bc6dbeb8f
amaury <carre.amaury@gmail.com>
2020-12-18 09:15:37 +0100

    fenetre finit

--- canvas.py
+++ canvas.py
@@ -3,36 +3,28 @@
 """
 Created on Fri Dec 18 08:11:57 2020
 
-@author: amaury
+@author: amaury et matthieu
 """
-
+#---------fenetre tkinter-------------------
 from tkinter import Tk, Label, StringVar, Entry, Button, Canvas, PhotoImage, Frame
 
 fenetre=Tk()
 fenetre.title('Space Invaders')
+fenetre.geometry("1500x700")
 
-canevas = Canvas(fenetre, bg='white', height=300, width=320)
-canevas.pack(side='top')
-
+canevas = Canvas(fenetre, bg='white', height=800, width=800)
+canevas.pack(side='left')
+#---------background--------------
 photo=PhotoImage(file="image/Yavin_4.png")
-image_pendu = Label(canevas, image=photo, border=0)
-image_pendu.pack()
+image_space = Label(canevas, image=photo, border=0)
+image_space.pack()
 
-Proposition= StringVar()
-Champ=Entry(fenetre, textvariable = Proposition, bg='light sky blue')
-Champ.pack(side='bottom',padx=5,pady=5)
-
-MotEnCour=Label(fenetre, text='hello')
-MotEnCour.pack(side='left',padx=10,pady=10)
-VotreLettre=Label(fenetre, text="Proposez une lettre :")
-VotreLettre.pack(side='left',padx=5,pady=5)
-
-boutonVal=Button(fenetre, text ='Valider')
-boutonVal.pack(side='bottom', padx=5, pady=5)
+#--------bouton------------------
+boutonPlay=Button(fenetre, text ='New Game')
+boutonPlay.pack(side='top', padx=5, pady=5)
 
 boutonQuit=Button(fenetre, text ='Abandonner', command=fenetre.destroy)
-boutonQuit.pack(side='right', padx=5, pady=5)
-
+boutonQuit.pack(side='top', padx=5, pady=5)
 
 fenetre.mainloop()
 fenetre.destroy()
